da87f976aef41fdb9ea8dce54369f85bbb7a0d4c
sparmira <aliceops09@gmail.com>
2025-10-29 20:53:02 +0600

    video-#4,  Создаем компонент пицц и категории, рендер списка

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -1,43 +1,45 @@
 import React from "react"
 
-export default function PizzaBlock() {
-const [pizzaCount, setPizzaCount] = React.useState(0)
+export default function PizzaBlock(props) {
+const typeNames = ['тонкое', 'традиационное']
 
-const onClickAdd = () => {
-    setPizzaCount(pizzaCount + 1)
-}
+const [activeType, setActiveType] = React.useState(0)
+const [activeSize, setActiveSize] = React.useState(0)
 
     return(
         <div className="pizza-block">
               <img
                 className="pizza-block__image"
-                src="https://media.dodostatic.net/image/r:233x233/01995c42f704740cae3253e3af6994dc.avif"
+                src={props.imageUrl}
                 alt="Pizza"
               />
-              <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+              <h4 className="pizza-block__title">{props.title}</h4>
 
               <div className="pizza-block__selector">
                 <ul>
-                  <li className="active">тонкое</li>
-                  <li>традиционное</li>
+                  {
+                    props.types.map((typeId) => (
+                      <li onClick={() => setActiveType(typeId)} className={activeType === typeId ? "active" : ""} >{typeNames[typeId]}</li>
+                    ))
+                  }
                 </ul>
                 <ul>
-                  <li className="active">26 см.</li>
-                  <li>30 см.</li>
-                  <li>40 см.</li>
+                  {
+                    props.sizes.map((size, index) => <li onClick={() => setActiveSize(index)} className={activeSize === index ? "active" : ""} >{size} см.</li>)
+                  }
                 </ul>
               </div>
 
               <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от 395 ₽</div>
-                <button onClick={onClickAdd} className="button button--outline button--add">
+                <div className="pizza-block__price">от {props.price} ₽</div>
+                <button className="button button--outline button--add">
                   <img
                     src="/img/plus.svg"
                     alt=""
                     style={{ marginRight: "0.5rem" }}
                   />
                   <span>Добавить</span>
-                  <i>{pizzaCount}</i>
+                  <i>0</i>
                 </button>
               </div>
             </div>
