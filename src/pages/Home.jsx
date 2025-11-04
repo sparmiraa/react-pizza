1049e1d15c8c5367a6932024ed18824533128040
sparmira <aliceops09@gmail.com>
2025-11-04 21:34:29 +0600

    video-#9, Разрабатываем пагинацию и поиск пицц

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,15 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import Pagination from "../components/Pagination/Pagination";
 
 const EMPTY_SKELETONS = [...new Array(8)];
 
-export default function Home() {
+export default function Home({ searchValue }) {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoryId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(1);
   const [sortType, setSortType] = React.useState({
     name: "популярности",
     sortProperty: "rating",
@@ -22,9 +24,10 @@ export default function Home() {
     const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
     const sortBy = sortType.sortProperty.replace("-", "");
     const category = categoryId > 0 ? `category=${categoryId}` : "";
+    const search = searchValue ? `&search=${searchValue}` : "";
 
     fetch(
-      `https://690399efd0f10a340b250ab6.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+      `https://690399efd0f10a340b250ab6.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`
     )
       .then((res) => res.json())
       .then((arr) => {
@@ -32,7 +35,11 @@ export default function Home() {
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
-  }, [categoryId, sortType]);
+  }, [categoryId, sortType, searchValue, currentPage]);
+
+  const pizzas = items.map((obj) => <PizzaBlock {...obj} key={obj.id} />);
+
+  const skeletons = EMPTY_SKELETONS.map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className="container">
@@ -46,11 +53,8 @@ export default function Home() {
 
       <h2 className="content__title">Все пиццы</h2>
 
-      <div className="content__items">
-        {isLoading
-          ? EMPTY_SKELETONS.map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
+      <Pagination onChangePage={(number) => setCurrentPage(number)} />
     </div>
   );
 }
