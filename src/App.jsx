c4c8e828c33a3032f42f135b78563e8f593cd559
sparmira <aliceops09@gmail.com>
2025-11-09 15:56:24 +0600

    video-#15, Сохраняем параметры фильтрации в URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,23 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 
 
 import React from "react";
 
-export const SearchContext = React.createContext("");
 
 export default function App() {
-  const [searchValue, setSearchValue] = React.useState("");
-
   return (
     <div className="wrapper">
-      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
-        <Header/>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </SearchContext.Provider>
+      <Header/>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit";
+import {configureStore} from "@reduxjs/toolkit";
 import filter from "./slices/filterSlice";
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     filter,
   },
 });
+
+export default store;
