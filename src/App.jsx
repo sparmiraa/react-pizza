8f665ae75db8b5500c032485eb95d751e14d461f
Myroslav Kosiuk <myroslav_kosiuk@epam.com>
2025-11-12 16:38:18 +0300

    video-#16, Создаем Redux-логику для корзины, доп. верстка

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,34 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
-import { Routes, Route } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import {Routes, Route} from "react-router-dom";
+import {cssTransition, ToastContainer} from "react-toastify";
 
 import React from "react";
+
 
 export default function App() {
   return (
     <div className="wrapper">
-      <Header />
+      <Header/>
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<Home/>}/>
+          <Route path="/cart" element={<Cart/>}/>
+          <Route path="*" element={<NotFound/>}/>
         </Routes>
       </div>
 
       <ToastContainer
         position="top-right"
-        autoClose={2500}
+        autoClose={1500}
         hideProgressBar
         theme="dark"
-        limit={3}
+        limit={2}
         newestOnTop={false}
         pauseOnFocusLoss
         draggable
+        closeButton={false}
         toastClassName="toast-item"
         className="toast-container"
       />
