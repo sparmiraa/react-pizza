d1d2e816ffa1dd338784fc2ec19a19ba8ae57c7a
Myroslav Kosiuk <myroslav_kosiuk@epam.com>
2025-11-12 16:32:17 +0300

    video-#16, Создаем Redux-логику для корзины, доп. верстка

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -18,39 +18,39 @@ const cartSlice = createSlice({
   reducers: {
     addItem(state, action) {
       const findItem = state.items.find((item) => isEqualItem(item, action.payload));
-    
+
       if (findItem) {
         findItem.count++;
         state.totalPrice += findItem.price;
       } else {
         state.items.push({ ...action.payload, count: 1 });
-        state.totalPrice += action.payload.price; // ⬅️ и тут
+        state.totalPrice += action.payload.price;
       }
     },
-    
+
     addItemCountById(state, action) {
       const item = state.items.find((item) => isEqualItem(item, action.payload));
-      if (!item) return;
+      if (!item || item.count >= 9) return;
       item.count++;
       state.totalPrice += item.price;
     },
-    
+
     minusItemCountById(state, action) {
       const item = state.items.find((item) => isEqualItem(item, action.payload));
       if (!item || item.count <= 1) return;
       item.count--;
       state.totalPrice -= item.price;
     },
-    
+
     removeItemById(state, action) {
       const item = state.items.find((item) => isEqualItem(item, action.payload));
       if (!item) return;
-    
-      
+
+
       state.totalPrice -= item.price * item.count;
       state.items = state.items.filter((i) => !isEqualItem(i, action.payload));
     },
-    
+
     clearItems(state) {
       state.items = [];
       state.totalPrice = 0;
